51331070f68cb806c93e2986b43a95448afe34bb
Ravikumar Chavva <chavvaravikumarreddy2004@gmail.com>
2024-07-15 12:56:10 +0530

    vs added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,6 +20,9 @@ const config = {
       },
     },
     extend: {
+      screens: {
+        'vs': {'raw': '(max-height: 500px)'}, 
+      },
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
       },
