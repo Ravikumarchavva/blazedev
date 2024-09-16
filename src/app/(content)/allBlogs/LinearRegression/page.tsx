2aab536e1a7f38f3cab62090b8595379cb034afb
Ravikumar Chavva <chavvaravikumarreddy2004@gmail.com>
2024-09-16 13:30:07 +0530

    blog

--- src/app/(content)/allBlogs/LinearRegression/page.tsx
+++ src/app/(content)/allBlogs/LinearRegression/page.tsx
@@ -9,7 +9,7 @@ const LinearRegression = () => {
   return (
     <div className="project w-full px-[5vw] portrait:px-0 flex flex-col pt-[10vh] portrait:pt-[5vh] items-center justify-center min-h-screen">
       {/* Example MDX Component */}
-      <div className="w-full mb-10 bg-primary shadow-2xl prose prose-lg max-w-none p-[5vw] text-white prose-headings:text-white prose-a:text-white prose-strong:text-white prose-code:text-white portrait:px-[5vw] portrait:mb-0">
+      <div className="w-full max-w-[100vw] mb-10 bg-primary shadow-2xl prose prose-lg p-[5vw] text-white prose-headings:text-white prose-a:text-white prose-strong:text-white prose-code:text-white portrait:px-[5vw] portrait:mb-0">
         <Example />
       </div>
     </div>
